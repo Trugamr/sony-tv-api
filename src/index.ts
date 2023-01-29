e5d3563050d6c8737102c9a6250b210d60ff5e41
Trugamr <trugamr@live.in>
2023-01-29 19:19:10 +0530

    feature: add method to change power status

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,77 @@
 import ky from 'ky-universal'
 import type { KyInstance } from 'ky/distribution/types/ky'
 import type {
-  GetCurrentTimeResult,
-  GetPowerStatusResult,
   SonyTvApiOptions,
+  GetCurrentTimeResult,
+  GetJsonBodyOptions,
+  GetPowerStatusResult,
+  SetPowerStatusOptions,
+  SetPowerStatusResult,
+  GetRemoteControllerInfoResult,
 } from './types'
 
 // @TODO: Make method to build json bodies to call methods
 
 export class SonyTvApi {
   #id = 1
+  #psk?: string
   #client: KyInstance
 
-  constructor(options: SonyTvApiOptions) {
+  constructor({ psk, host }: SonyTvApiOptions) {
+    this.#psk = psk
     this.#client = ky.create({
-      prefixUrl: options.host,
+      prefixUrl: host,
+      hooks: {
+        beforeRequest: [
+          request => {
+            // Add pre shared key to headers before every request
+            if (this.#psk) {
+              request.headers.set('X-AUTH-PSK', this.#psk)
+            }
+            return request
+          },
+        ],
+      },
     })
   }
 
   async getCurrentTime() {
+    const json = this.#getJsonBody({ method: 'getCurrentTime' })
     return this.#client
-      .post('sony/system', {
-        json: {
-          id: this.#id,
-          method: 'getCurrentTime',
-          version: '1.0',
-          params: [''],
-        },
-      })
+      .post('sony/system', { json })
       .json<GetCurrentTimeResult>()
   }
 
   async getPowerStatus() {
+    const json = this.#getJsonBody({ method: 'getPowerStatus' })
     return this.#client
-      .post('sony/system', {
-        json: {
-          id: this.#id,
-          method: 'getPowerStatus',
-          version: '1.0',
-          params: [''],
-        },
-      })
+      .post('sony/system', { json })
       .json<GetPowerStatusResult>()
+  }
+
+  async setPowerStatus({ status }: SetPowerStatusOptions) {
+    const json = this.#getJsonBody({
+      method: 'setPowerStatus',
+      params: [{ status }],
+    })
+    return this.#client
+      .post('sony/system', { json })
+      .json<SetPowerStatusResult>()
+  }
+
+  async getRemoteControllerInfo() {
+    const json = this.#getJsonBody({ method: 'getRemoteControllerInfo' })
+    return this.#client
+      .post('sony/system', { json })
+      .json<GetRemoteControllerInfoResult>()
+  }
+
+  #getJsonBody({
+    id = this.#id,
+    method,
+    version = '1.0',
+    params = [''],
+  }: GetJsonBodyOptions) {
+    return { id, method, version, params }
   }
 }
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
 export type SonyTvApiOptions = {
+  psk?: string
   host: string | URL
 }
 
@@ -7,7 +8,28 @@ type Result<T> = {
   id: number
 }
 
+export type GetJsonBodyOptions = {
+  id?: number
+  method:
+    | 'getCurrentTime'
+    | 'getPowerStatus'
+    | 'setPowerStatus'
+    | 'getRemoteControllerInfo'
+  version?: string
+  params?: unknown[]
+}
+
 export type GetCurrentTimeResult = Result<[string]>
 
 // @TODO: Find out values for "status"
 export type GetPowerStatusResult = Result<[{ status: 'active' | 'standby' }]>
+
+export type SetPowerStatusOptions = {
+  status: boolean
+}
+
+export type SetPowerStatusResult = Result<[]>
+
+export type GetRemoteControllerInfoResult = Result<
+  [{ bundled: boolean; type: string }, Array<{ name: string; value: string }>]
+>
